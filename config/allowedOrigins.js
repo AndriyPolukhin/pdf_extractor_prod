a6feea9cefad6730147808442a9d016210e1de85
Andriy Polukhin <andriy.polukhin@gmail.com>
2024-11-18 18:09:13 +0200

    updated origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,7 @@ export const allowedOrigins = [
 	'http://127.0.0.1:5000',
 	'http://localhost:3000',
 	'http://localhost:5000',
+	'https://pdf-extractor-prod.onrender.com',
 	'0.0.0.0/1',
 ]
 // fetch('http://localhost:3500')
